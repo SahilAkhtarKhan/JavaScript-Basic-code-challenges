203fa64bab856730ec53dc4dd3d1685895a000d0
unknown <khansahil1886@gmail.com>
2022-05-20 23:36:37 +0530

    challenge-03 updated with new program

--- challenge03.js
+++ challenge03.js
@@ -1,11 +1,10 @@
 // Challenge -03  -----------if the bill is between 50 & 300 then the tip will be 15% and if more than 300 than the tip will be 20%
 
 let bill = 275;
-let tip =
-  bill >= 50 && bill <= 300
-    ? `${(tip = bill * 0.15)}`
-    : `${(tip = bill * 0.2)}`;
-console.log(`Bill : ${bill}, Tip : ${tip}, & Total : ${bill + tip}`);
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(typeof `Bill : ${bill}, Tip : ${tip}, & Total : ${bill + tip}`);
+
+_____________________________________________________________________________________;
 
 // Calculate the retirement years with current age using function method
 
